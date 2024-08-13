4a028c08fd44ab635cef44d36ec23dfc136ef8ea
mabupro <mabupro787@gmail.com>
2024-08-13 14:55:19 +0900

    update/use next-image

--- app/[maturi]/page.tsx
+++ app/[maturi]/page.tsx
@@ -5,18 +5,18 @@ import EventPeriod from "@/components/elements/EventPeriod"
 import NewsButton from "@/components/elements/NewsButton"
 import MainButton from "@/components/elements/MainButton"
 import GoogleMap from "@/components/elements/GoogleMap"
+import Image from "next/image"
 
 // データの型定義
 interface EventData {
     [key: number]: string
 }
-
 interface NewsData {
     [key: number]: string
 }
 
 const formatDriveUrl = (url: string) => {
-    if (!url) return
+    if (!url) return ''
 
     // https://zenn.dev/collabostyle/articles/2060bfc3fd68be 解決方法
     const fileIdMatch = url.match(/(?<=id=)[-\w]{25,}/)
@@ -28,18 +28,19 @@ export default async function Maturi({ params }: { params: { maturi: string } })
     const eventData = data[0]
     const newsData = data[1]
 
-    {/* TODO: データが存在する時だけ表示したい */ }
+    // TODO: データが存在する時だけ表示したい
     if (params.maturi === "Suito") {
         return (
             <>
                 {/* TODO: Header名を動的にしたい */}
                 <Header title="Demo: Suito Festival" />
                 <div className="bg-teal-500 pt-20">
-                    <img
+                    <Image
                         className="mx-auto mt-8 w-4/5 h-72 justify-center rounded-2xl bg-white"
-                        src={eventData[1] ? formatDriveUrl(eventData[1][3]) : ''}
-                        // src={eventData[1] ? eventData[1][3] : ''}
+                        src={eventData[1] ? formatDriveUrl(eventData[1][3]) : '/default-image-path.jpg'} // Provide a default image path
                         alt="Festival Image"
+                        width={400}
+                        height={200}
                     />
                     <div className="mt-8 pb-3">
                         <Subtitle subtitle="開催期間" color="white" />
@@ -87,14 +88,13 @@ export default async function Maturi({ params }: { params: { maturi: string } })
                 <div className="bg-white py-12">
                     <Subtitle subtitle="まつりについて" color="" />
                     {/* TODO: 必要かどうかや、名前も動的にしたい */}
-
                     <div className="cursor-pointer">
-                        <img className="mx-auto mt-8 w-4/5 h-48 justify-center rounded-md bg-slate-300" />
+                        <Image className="mx-auto mt-8 w-4/5 h-48 justify-center rounded-md bg-slate-300" alt="祭りの歴史" src="/path-to-image.jpg" width={640} height={360} />
                         <p className="text-xl font-semibold text-center pr-36 mt-3 text-gray-700">水都まつりの歴史</p>
                     </div>
                     {/* TODO: 必要かどうかや、名前も動的にしたい */}
                     <div className="cursor-pointer">
-                        <img className="mx-auto mt-8 w-4/5 h-48 justify-center rounded-md bg-slate-300" />
+                        <Image className="mx-auto mt-8 w-4/5 h-48 justify-center rounded-md bg-slate-300" alt="祭りの概要" src="/path-to-image.jpg" width={640} height={360} />
                         <p className="text-xl font-semibold text-center pr-36 mt-3 text-gray-700">水都まつりの概要</p>
                     </div>
                 </div>
@@ -102,4 +102,3 @@ export default async function Maturi({ params }: { params: { maturi: string } })
         )
     }
 }
-
